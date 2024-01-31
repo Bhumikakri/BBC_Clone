b55a4f5562f74c752d960550246014e25566be34
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-31 18:00:42 +0530

    some functionality left

--- src/Firebase/FirebaseSetup.js
+++ src/Firebase/FirebaseSetup.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth , GoogleAuthProvider } from "firebase/auth";
+import { getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOrAUqCJa6KaSQrV8U0VIHI7a-LjybWaQ",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider(app)
+export const database = getFirestore(app)
